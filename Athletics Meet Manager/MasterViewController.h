e70ba736a0eeb21dd25d3dfc257a9b3ef0eafca2
Ailsa Huysamen <ailsahuysamen@ailsas-mbp.home>
2015-05-22 20:13:45 +0100

    Initial Commit

---
+++ Athletics Meet Manager/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Athletics Meet Manager
+//
+//  Created by Ailsa Huysamen on 22/05/2015.
+//  Copyright (c) 2015 rudi huysamen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
---
+++ Athletics Meet ManagerTests/Athletics_Meet_ManagerTests.m
@@ -0,0 +1,40 @@
+//
+//  Athletics_Meet_ManagerTests.m
+//  Athletics Meet ManagerTests
+//
+//  Created by Ailsa Huysamen on 22/05/2015.
+//  Copyright (c) 2015 rudi huysamen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Athletics_Meet_ManagerTests : XCTestCase
+
+@end
+
+@implementation Athletics_Meet_ManagerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
