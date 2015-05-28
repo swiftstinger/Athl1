8583c63cff23ab28f97a7c066330d90ed7cffb01
swiftstinger <ailsahuysamen@ailsas-mbp.home>
2015-05-28 13:05:55 +0100

    Structure

---
+++ Athletics Meet Manager/EnterResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  EnterResultsViewController.h
+//  Athletics Meet Manager
+//
+//  Created by Ailsa Huysamen on 28/05/2015.
+//  Copyright (c) 2015 rudi huysamen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EnterResultsViewController : UITableViewController
+
+@end
---
+++ Athletics Meet Manager/FinalResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FinalResultsViewController.h
+//  Athletics Meet Manager
+//
+//  Created by Ailsa Huysamen on 28/05/2015.
+//  Copyright (c) 2015 rudi huysamen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FinalResultsViewController : UITableViewController
+
+@end
