2c91bed038683fac49a5a4b1c8ea7ed8873be922
swiftstinger <ailsahuysamen@ailsas-mbp.home>
2015-05-29 19:30:46 +0100

    swipe test

--- Athletics Meet Manager/MasterViewController.m
+++ Athletics Meet Manager/MasterViewController.m
@@ -26,7 +26,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     
    
-    self.navigationItem.leftBarButtonItem = self.editButtonItem;
+   // self.navigationItem.leftBarButtonItem = self.editButtonItem;
  /**
     UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
     self.navigationItem.rightBarButtonItem = addButton;
